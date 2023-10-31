c4108b528757d7f90ba617dae49783d571798fe1
PawanSirsat <p1.sirsat1998@gmail.com>
2023-10-31 23:51:48 +0530

    Commit

--- git.js
+++ git.js
@@ -1,7 +1,45 @@
 let githubToken = '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'
 let githubUsername = 'PawanSirsat'
+let squares = document.querySelector('.squares')
 
-const query = `
+document.addEventListener('DOMContentLoaded', function () {
+  const fetchButton = document.getElementById('fetch-button')
+  const usernameInput = document.getElementById('username')
+  const userInfoDiv = document.getElementById('user-info')
+
+  function getUserData(username) {
+    if (username) {
+      fetch(`https://api.github.com/users/${username}`)
+        .then((response) => response.json())
+        .then((data) => {
+          userInfoDiv.innerHTML = `
+            <h2>${data.name}</h2>
+            <img src="${data.avatar_url}" alt="${data.login}" width="100">
+            <p>Username: ${data.login}</p>
+            <p>Followers: ${data.followers}</p>
+            <p>Following: ${data.following}</p>
+          `
+        })
+        .catch((error) => {
+          userInfoDiv.innerHTML = `<p>Error: User not found</p>`
+        })
+    }
+  }
+
+  fetchButton.addEventListener('click', function () {
+    const username = usernameInput.value
+    squares.innerHTML = ''
+    githubUsername = username
+    getGitHubContributions()
+    getUserData(username)
+  })
+
+  // Automatically load data for a default GitHub user when the page loads
+  const defaultUsername = 'PawanSirsat'
+  getUserData(defaultUsername)
+})
+
+let query = `
         query($userName: String!) {
             user(login: $userName) {
                 contributionsCollection {
@@ -43,7 +81,6 @@ async function getGitHubContributions() {
         (week) => week.contributionDays
       )
 
-    const squares = document.querySelector('.squares')
     contributions.forEach((contribution) => {
       const date = new Date(contribution.date)
       const day = date.getDate()
