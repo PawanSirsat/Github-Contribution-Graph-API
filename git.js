036fa1155891d88a115b8d18f40933205c216b4f
PawanSirsat <p1.sirsat1998@gmail.com>
2023-11-01 02:46:45 +0530

    Commit

--- git.js
+++ git.js
@@ -1,5 +1,5 @@
 let githubToken =
-  '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'
+  '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'
 let githubUsername = 'PawanSirsat'
 let squares = document.querySelector('.squares')
 const months = [
